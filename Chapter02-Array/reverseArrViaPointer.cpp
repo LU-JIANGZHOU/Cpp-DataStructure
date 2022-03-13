08cf7172e5f893cbf90d140c9683b7b99e1aaa37
LU JIANGZHOU <jiangzhoulu@outlook.com>
2022-03-13 21:35:18 +0800

    updated Order table and linked list

--- Chapter02-Array/reverseArrViaPointer.cpp
+++ Chapter02-Array/reverseArrViaPointer.cpp
@@ -4,8 +4,57 @@ using namespace std;
 
 int fillArr(int [], int);
 void showArr(int [], int);
-int* reverseArr(int [], int);
+void reverseArr(int *, int *);
 
 int main(){
+    int n=5;
+    cin >>n;
+    int * myArr =new int[n];
 
+    int * pBegin = &myArr[0];
+    int * pEnd = &myArr[0] + (n - 1);
+
+    int iDim=fillArr(myArr,n);
+    showArr(myArr, iDim);
+
+    reverseArr(pBegin, pEnd);
+    showArr(myArr, iDim);
+
+    delete [] myArr;    
+    return 0;
 };
+
+int fillArr(int arr[], int n){
+    int iCount=0;
+    for (int i=0; i<n; i++){
+        cin >> arr[i];
+        if (!cin){
+            cin.clear();
+            while (cin.get() != '\n'){
+                continue;
+            }
+            break;
+        }
+        iCount++;
+    }
+    return iCount;
+}
+
+void showArr(int arr[], int n)
+{
+    for (int i = 0; i < n; i++){
+    cout << arr[i] << ' ';
+    }
+    cout << endl;
+}
+
+void reverseArr(int* arrBegin, int * arrEnd){
+    int temp;
+    while (arrBegin < arrEnd){
+        temp = *arrBegin;
+        *arrBegin = *arrEnd;
+        *arrEnd = temp;
+        arrBegin++;
+        arrEnd--;
+    }    
+}
